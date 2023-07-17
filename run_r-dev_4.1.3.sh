4a953e03bb81aa1652c31052673dffa51cb1f298
sunamask <nas@shenkebio.com>
2023-07-17 09:01:38 +0800

    feat: add new R version

---
+++ run_r-dev_4.1.3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+IMAGE_VERSION=${1:-0.0.1.9003}
+PORT_RSTUDIO=8787
+DEFAULT_USER="r"
+CONTAINER_NAME="r-dev_4.1.3"
+
+docker run \
+    --name $CONTAINER_NAME \
+    --rm -detach \
+    -p $PORT_RSTUDIO:8787 \
+    --mount type=bind,source=$HOME/renv_root,target=/renv_root \
+    --mount type=bind,source=$HOME/proj/,target=/home/$DEFAULT_USER/proj \
+    --mount type=bind,source=$HOME/work/,target=/home/$DEFAULT_USER/work \
+    --mount type=bind,source="$(pwd)"/server_settings/rstudio/XDG_CONFIG_HOME/,target=/home/$DEFAULT_USER/.config/rstudio \
+    --mount type=bind,source="$(pwd)"/server_settings/rstudio/local_share/,target=/home/$DEFAULT_USER/.local/share/rstudio \
+    --mount type=bind,source="$(pwd)"/scripts/docker_entrypoint_r_dev.sh,target=/home/$DEFAULT_USER/.config/docker_entrypoint.sh \
+    -e ROOT=true \
+    -e DISABLE_AUTH=true \
+    -e PASSWORD=1234 \
+    -e USERID=`id -u $USER` \
+    -e GROUPID=`id -g $USER` \
+    -e RENV_PATHS_ROOT=/renv_root \
+    --network r2db \
+    --entrypoint /home/$DEFAULT_USER/.config/docker_entrypoint.sh \
+    sunamask/r-dev_4.1.3:$IMAGE_VERSION
