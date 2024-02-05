67118ea73fbf15a9c1a8782eec70e60ffe762e13
sunamask <nas@shenkebio.com>
2024-02-05 09:56:18 +0800

    update r-dev_4.2.3

--- build_4.2.3.sh
+++ build_4.2.3.sh
@@ -3,6 +3,6 @@
 docker build \
     --progress=plain \
     -f ./dockerfiles/r-dev_4.2.3.dockerfile \
-    -t sunamask/r-dev_4.2.3:$1 \
+    -t quay.io/giganticdiaper/r-dev_4.2.3:$1 \
     . 2>&1 | \
 tee ./build_logs/sunamask_r-dev_4.2.3_$1.log
